0163778620e969b793d801520e025caa2bc0260e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-03-08 10:58:38 +0100

    dev: server ok

--- back/server.js
+++ back/server.js
@@ -1,3 +1,12 @@
 const express = require("express");
 
-console.log("coucou je suis toto");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
