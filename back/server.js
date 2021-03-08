bdd365c3dd57903bbf5df2eb0dc99fdee586992b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-03-08 12:00:35 +0100

    dev: added static and serve-index

--- back/server.js
+++ back/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const serveIndex = require("serve-index");
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  console.log("req.url: ", req.url);
+  next();
 });
+
+app.use(express.static("."));
+app.use(serveIndex(".", { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
