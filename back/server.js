63522e62a0181042787704d48cbff6b131bb72fe
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-03-08 12:10:54 +0100

    dev: added public directory

--- back/server.js
+++ back/server.js
@@ -3,14 +3,15 @@ const serveIndex = require("serve-index");
 
 const app = express();
 const port = 3000;
+const www = "./public";
 
 app.use((req, res, next) => {
   console.log("req.url: ", req.url);
   next();
 });
 
-app.use(express.static("."));
-app.use(serveIndex(".", { icons: true }));
+app.use(express.static(www));
+app.use(serveIndex(www, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
