1cd2fedc9308ba2a59ff346271bf166a42c5b14b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-03-09 14:31:04 +0100

    dev: added add route

--- front/src/app/stock/stock-routing.module.ts
+++ front/src/app/stock/stock-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
 import { StockComponent } from './stock.component';
 
-const routes: Routes = [{ path: '', component: StockComponent }];
+const routes: Routes = [
+  { path: '', component: StockComponent },
+  { path: 'add', component: AddComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class StockRoutingModule { }
+export class StockRoutingModule {}
--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
+import { AddComponent } from './add/add.component';
 
 
 @NgModule({
-  declarations: [StockComponent],
+  declarations: [StockComponent, AddComponent],
   imports: [
     CommonModule,
     StockRoutingModule
