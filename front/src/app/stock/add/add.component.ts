f4ade4b065090502e62c38b003455ee534b51faa
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-03-09 16:12:00 +0100

    dev: added reactivesform

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('toto', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    price: new FormControl('1.23', [Validators.required]),
+    qty: new FormControl('100', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -4,13 +4,10 @@ import { CommonModule } from '@angular/common';
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
 import { AddComponent } from './add/add.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [StockComponent, AddComponent],
-  imports: [
-    CommonModule,
-    StockRoutingModule
-  ]
+  imports: [CommonModule, StockRoutingModule, ReactiveFormsModule],
 })
-export class StockModule { }
+export class StockModule {}
