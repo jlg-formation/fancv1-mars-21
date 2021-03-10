588f9e67a2e8c2db40da8f405dc8f96c16ce29c6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-03-10 14:14:45 +0100

    dev: added options on pipe

--- front/src/app/widget/ellipsis.pipe.ts
+++ front/src/app/widget/ellipsis.pipe.ts
@@ -4,9 +4,9 @@ import { Input, Pipe, PipeTransform } from '@angular/core';
   name: 'ellipsis',
 })
 export class EllipsisPipe implements PipeTransform {
-  transform(value: string, ...args: unknown[]): string {
-    if (value.length > 10) {
-      return value.substr(0, 10) + '...';
+  transform(value: string, length = 10, ellipsis = '...'): string {
+    if (value.length > length) {
+      return value.substr(0, length) + ellipsis;
     }
     return value;
   }
