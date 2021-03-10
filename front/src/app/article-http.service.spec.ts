8aa8a70742795f9caf14461c94197e0ad3b840cf
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-03-10 15:27:05 +0100

    dev: added add service http test

--- front/src/app/article-http.service.spec.ts
+++ front/src/app/article-http.service.spec.ts
@@ -1,19 +1,73 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ArticleHttpService } from './article-http.service';
 
 describe('ArticleHttpService', () => {
   let service: ArticleHttpService;
+  let http: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(ArticleHttpService);
+    http = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    http.verify();
   });
 
   it('should be created', () => {
+    // simulate http return 200 OK
+    const req = http.expectOne('/api/articles');
+    expect(req.request.method).toBe('GET');
+    req.flush([]);
+    expect(service).toBeTruthy();
+  });
+
+  it('should refresh with error', () => {
+    // simulate http return 404 Not found
+    const req = http.expectOne('/api/articles');
+    expect(req.request.method).toBe('GET');
+    req.flush(null, { status: 404, statusText: 'Not Found ' });
+    expect(service).toBeTruthy();
+  });
+
+  it('should test add in error', () => {
+    // simulate http return 404 Not found
+    const req = http.expectOne('/api/articles');
+    expect(req.request.method).toBe('GET');
+    req.flush(null, { status: 404, statusText: 'Not Found ' });
+
+    service.add({ name: 'toto', price: 1, qty: 1 });
+
+    const req2 = http.expectOne('/api/articles');
+    expect(req2.request.method).toBe('POST');
+    req2.flush(null, { status: 404, statusText: 'Not Found ' });
+
+    expect(service).toBeTruthy();
+  });
+
+  it('should test add in success', () => {
+    const req = http.expectOne('/api/articles');
+    expect(req.request.method).toBe('GET');
+    req.flush(null, { status: 404, statusText: 'Not Found ' });
+
+    service.add({ name: 'toto', price: 1, qty: 1 });
+
+    const req2 = http.expectOne('/api/articles');
+    expect(req2.request.method).toBe('POST');
+    req2.flush('');
+
+    const req3 = http.expectOne('/api/articles');
+    expect(req3.request.method).toBe('GET');
+    req3.flush([]);
+
     expect(service).toBeTruthy();
   });
 });
--- front/src/app/stock/stock.component.spec.ts
+++ front/src/app/stock/stock.component.spec.ts
@@ -1,3 +1,4 @@
+import { WidgetModule } from './../widget/widget.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StockComponent } from './stock.component';
@@ -8,9 +9,9 @@ describe('StockComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StockComponent ]
-    })
-    .compileComponents();
+      imports: [WidgetModule],
+      declarations: [StockComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
