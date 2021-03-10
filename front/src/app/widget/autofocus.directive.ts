ffa8872d34ca1269e6f7658bd6b1c703614fcaf1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-03-10 12:19:24 +0100

    dev: added fake input on directive

--- front/src/app/widget/autofocus.directive.ts
+++ front/src/app/widget/autofocus.directive.ts
@@ -1,13 +1,18 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appAutofocus]',
 })
 export class AutofocusDirective implements OnInit {
+  @Input() toto!: string;
+  @Input('appAutofocus') qq!: string;
+
   constructor(private elt: ElementRef<HTMLElement>) {
     console.log('autofocus instantiated.');
   }
   ngOnInit(): void {
     this.elt.nativeElement.focus();
+    console.log('this.toto', this.toto);
+    console.log('this.qq', this.qq);
   }
 }
